2115024891243f2a150ed3cf8f5f630bf16cca7f
amir <amirkhafane87@gmail.com>
2025-09-08 23:00:24 +0330

    feat: Set the app up for the development to start

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react-swc';
-import mkcert from 'vite-plugin-mkcert';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: "/reactjs-template/",
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern',
+        api: "modern",
       },
     },
   },
   plugins: [
+    basicSsl(),
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
     react(),
@@ -23,15 +24,12 @@ export default defineConfig({
     // Creates a custom SSL certificate valid for the local machine.
     // Using this plugin requires admin rights on the first dev-mode launch.
     // https://www.npmjs.com/package/vite-plugin-mkcert
-    process.env.HTTPS && mkcert(),
   ],
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-  publicDir: './public',
+  publicDir: "./public",
   server: {
-    // Exposes your dev server and makes it accessible for the devices in the same network.
     host: true,
   },
 });
-
